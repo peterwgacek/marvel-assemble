2e6e348bb1ea6295fdbc09dc213661c2deaa6cb5
Peter Gacek <peterwgacek@gmail.com>
2020-12-08 11:22:18 -0500

    adds password hash

--- models/user.js
+++ models/user.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 6;
 
 const userSchema = new Schema({
   name: String,
   email: {
     type: String,
     required: true,
+    // Makes it so email field will accept lower or uppercase on email 
     lowercase: true,
+    //Doesn't create duplicates
     unique: true
   },
   password: String
@@ -14,12 +18,27 @@ const userSchema = new Schema({
   timestamps: true
 });
 
+//Schema to remove password when serializing
 userSchema.set('toJSON', {
   transform: function (doc, ret) {
     // remove the password property when serializing doc to JSON
     delete ret.password;
     return ret;
   }
+});
+
+
+// Shcema to hash the user's password
+userSchema.pre('save', function (mext) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  // password has been changed - salt and hash it
+  bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
+    if (err) return next(err);
+    // replace the user provided password with the hash
+    user.password = hash;
+    next();
+  });
 });
 
 module.exports = mongoose.model('User', userSchema);
